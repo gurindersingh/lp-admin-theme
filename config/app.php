d9f009230c5df62d9a4234a17ffe8efab60b927c
Gurinder Chauhan <gurinderchauhan@gmail.com>
2016-11-09 15:48:20 -0500

    Removed laravelcollective/html & intervention/image package

--- config/app.php
+++ config/app.php
@@ -167,9 +167,6 @@ return [
          * Package Service Providers...
          */
 
-        Collective\Html\HtmlServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
-
         /*
          * Application Service Providers...
          */
@@ -226,10 +223,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];

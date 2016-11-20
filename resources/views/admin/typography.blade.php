414a7c10f52824ac6bee505ba30ff6506e45158f
Gurinder Chauhan <gurinderchauhan@gmail.com>
2016-11-19 19:30:42 -0500

    Fix pagination for Laravel

--- resources/views/admin/typography.blade.php
+++ resources/views/admin/typography.blade.php
@@ -64,15 +64,20 @@
                     </ul>
                     <h3>Pagination</h3>
                     <ul class="pagination">
-                        <li><a href="#"><<</a></li>
-                        <li><a href="#"><</a></li>
+                        <li><a href="#" rel="prev">«</a></li>
                         <li><a href="#">1</a></li>
-                        <li class="active"><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">8</a></li>
-                        <li><a href="#">></a></li>
-                        <li><a href="#">>></a></li>
+                        <li><a href="#">2</a></li> 
+                        <li class="disabled"><span>...</span></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li><a href="#">6</a></li>
+                        <li><a href="#">7</a></li>
+                        <li class="active"><span>8</span></li>
+                        <li><a href="#">9</a></li>
+                        <li><a href="#">10</a></li>
+                        <li><a href="#">11</a></li>
+                        <li><a href="#">12</a></li>
+                        <li><a href="#" rel="next">»</a></li>
                     </ul>
                 </main>
             </section>
